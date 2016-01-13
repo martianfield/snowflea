e4de81b0fc8b2b35d6fd31646429929fff18f324
Martin Imfeld <martin.imfeld@gmail.com>
2016-01-13 07:03:03 +0100

    method stubs added

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 'use strict';
 
+module.exports.createOne = require(__dirname + '/lib/create.js').createOne;
 module.exports.createMany = require(__dirname + '/lib/create.js').createMany;
 module.exports.readAll = require(__dirname + '/lib/read.js').readAll;
-//module.exports.update = require(__dirname + '/lib/update.js');
+module.exports.updateOne = require(__dirname + '/lib/update.js').updateOne;
+module.exports.updateMany = require(__dirname + '/lib/update.js').updateMany;
 module.exports.deleteOne = require(__dirname + '/lib/delete.js').deleteOne;
 module.exports.deleteAll = require(__dirname + '/lib/delete.js').deleteAll;
+
 
 
