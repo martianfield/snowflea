732c0975f6fee63bd2699458d6e0ac49bca1c78f
Martin Imfeld <martin.imfeld@gmail.com>
2016-01-13 12:04:50 +0100

    readOneById()

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 module.exports.createOne = require(__dirname + '/lib/create.js').createOne;
 module.exports.createMany = require(__dirname + '/lib/create.js').createMany;
 module.exports.readAll = require(__dirname + '/lib/read.js').readAll;
+module.exports.readOne = require(__dirname + '/lib/read.js').readOne;
+module.exports.readOneById = require(__dirname + '/lib/read.js').readOneById;
 module.exports.updateOne = require(__dirname + '/lib/update.js').updateOne;
 module.exports.updateMany = require(__dirname + '/lib/update.js').updateMany;
 module.exports.deleteOne = require(__dirname + '/lib/delete.js').deleteOne;
--- test/read_test.js
+++ test/read_test.js
@@ -8,15 +8,14 @@ const expect = chai.expect;
 const snowflea = require(__dirname + '/../index.js');
 const database = require(__dirname + '/../lib/database.js');
 
-
-
 describe("Read", () => {
+  let collection = 'snowflea_test_read';
   let created_items = [];
   before(() => {
     database.set('uri', 'mongodb://localhost:27017/test');
-    return snowflea.deleteAll('snowflea_test_read')
+    return snowflea.deleteAll(collection)
       .then((count) => {
-        return snowflea.createMany('snowflea_test_read', [
+        return snowflea.createMany(collection, [
           {"name": "Pip"},
           {"name": "Pop"},
           {"name": "Pap"}
@@ -31,10 +30,12 @@ describe("Read", () => {
   });
 
   it(('readAll()'), () => {
-    return snowflea.readAll('snowflea_test_read').should.eventually.have.length(3);
+    return snowflea.readAll(collection).should.eventually.have.length(3);
   });
 
-  it(('readOne()'), () => {
-    // TODO
+  it(('readOneById()'), () => {
+    let id = created_items[0]._id;
+    return expect(snowflea.readOneById(collection,id)).to.eventually.have.property('name', 'Pip');
+    return snowflea.readOneById(collection, id).should.eventually.have.property('name', 'Pip');
   })
 });
